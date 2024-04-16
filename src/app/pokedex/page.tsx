c3b89c591d51195913de7616fcfe8bbd5efdc85e
Juan Rivera <jjrr0213@gmail.com>
2024-04-15 21:59:57 -0600

    filter update

--- src/app/pokedex/page.tsx
+++ src/app/pokedex/page.tsx
@@ -119,92 +119,7 @@ const Pokedex = () => {
         </NavbarItem>
       </NavbarContent>
       </Navbar>
-      <div className="
-        px-5
-        w-full
-        pt-9 
-        space-x-5
-        flex justify-center items-center
-        md:px-2"
-      >
-        <Select 
-          name="status" 
-          id="Pokemons" 
-          label="Pokemons" 
-          defaultSelectedKeys={["All"]}
-          className="w-32 text-black font-fantasy"
-          radius="lg"
-          size="sm" 
-          isDisabled={reset ? true : false}
-          color="default" 
-          variant="faded" 
-          classNames={{
-            label:[
-              "text-black"
-            ]
-          }}
-          onChange={(e) => handleStatus(e)}
-        >
-            {filters.map(fil => {
-              return (
-                <SelectItem key={fil} value={fil} id={fil}>
-                 {fil}
-                </SelectItem>
-              )
-            })}
-        </Select>
-        <Select 
-          name="order" 
-          id="Order" 
-          label="Order" 
-          defaultSelectedKeys={["None"]} 
-          className="w-32 text-black font-fantasy"
-          radius="lg"
-          size="sm" 
-          isDisabled={reset ? true : false}
-          color="default" 
-          variant="faded"
-          classNames={{
-            label:[
-              "text-black"
-            ]
-          }}
-          onChange={(e) => handleStatus(e)}
-          >
-            {orders.map(ord => {
-              return (
-                <SelectItem key={ord} value={ord} id={ord}>
-                  {ord}
-                </SelectItem>
-              )
-            })}
-        </Select>
-        <Select 
-          name="poketypes" 
-          id="Types" 
-          label="Types" 
-          defaultSelectedKeys={["None"]} 
-          className="w-32 text-black font-fantasy"
-          radius="lg"
-          size="sm"
-          isDisabled={reset ? true : false}
-          color="default" 
-          variant="faded"
-          classNames={{
-            label:[
-              "text-black"
-            ]
-          }}
-          onChange = {(e) => handleStatus(e)}
-        >
-            {types.map(type => {
-              return (
-                <SelectItem key={type} value={type}  id={type}>
-                  {type}
-                </SelectItem>
-              )
-            })}
-        </Select>
+      <div className="flex flex-col items-center space-y-5 pt-6">
         <Button
           variant="faded"
           radius="lg"
@@ -215,6 +130,94 @@ const Pokedex = () => {
         >
           Filter
         </Button>
+        <div 
+          className="
+          px-5
+          w-full
+          
+          space-x-2
+          flex justify-center items-center
+          md:px-2 md:space-x-5"
+        >
+          <Select 
+            name="status" 
+            id="Pokemons" 
+            label="Pokemons" 
+            defaultSelectedKeys={["All"]}
+            className="w-32 text-black font-fantasy"
+            radius="lg"
+            size="sm" 
+            isDisabled={reset ? true : false}
+            color="default" 
+            variant="faded" 
+            classNames={{
+              label:[
+                "text-black"
+              ]
+            }}
+            onChange={(e) => handleStatus(e)}
+          >
+            {filters.map(fil => {
+              return (
+                <SelectItem key={fil} value={fil} id={fil}>
+                 {fil}
+                </SelectItem>
+              )
+            })}
+          </Select>
+          <Select 
+            name="order" 
+            id="Order" 
+            label="Order" 
+            defaultSelectedKeys={["None"]} 
+            className="w-32 text-black font-fantasy"
+            radius="lg"
+            size="sm" 
+            isDisabled={reset ? true : false}
+            color="default" 
+            variant="faded"
+            classNames={{
+              label:[
+                "text-black"
+              ]
+            }}
+            onChange={(e) => handleStatus(e)}
+            >
+              {orders.map(ord => {
+                return (
+                  <SelectItem key={ord} value={ord} id={ord}>
+                    {ord}
+                  </SelectItem>
+                )
+              })}
+          </Select>
+          <Select 
+            name="poketypes" 
+            id="Types" 
+            label="Types" 
+            defaultSelectedKeys={["None"]} 
+            className="w-32 text-black font-fantasy"
+            radius="lg"
+            size="sm"
+            isDisabled={reset ? true : false}
+            color="default" 
+            variant="faded"
+            classNames={{
+              label:[
+                "text-black"
+              ]
+            }}
+            onChange = {(e) => handleStatus(e)}
+          >
+              {types.map(type => {
+                return (
+                  <SelectItem key={type} value={type}  id={type}>
+                    {type}
+                  </SelectItem>
+                )
+              })}
+          </Select>
+        </div>
       </div>
       <div className="
         w-full
